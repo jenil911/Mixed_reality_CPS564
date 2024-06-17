88116e85a8e8ec9256fcfb703ea6ea61f03bf0b6
Your Name <jenilpandya911@gmail.com>
2024-06-17 18:27:41 -0400

    Lab 8

---
+++ Lab8/Lab8.m
@@ -0,0 +1,25 @@
+close all;
+clear all;
+clc;
+
+img = imread("tower.jpg");
+figure, imshow(img);
+
+sm = saliency(img);
+figure, imshow(sm,[]);
+
+[height, width] = size(sm);
+new_height = height*2/3;
+new_width = width*2/3;
+
+height_values = sum(sm');
+width_values = sum(sm);
+
+[~,ind1] = sort(height_values);
+[~,ind2] = sort(width_values);
+
+img(ind1(1:height - new_height),:,:) = [];
+figure, imshow(img);
+
+img(:,ind2(1:width - new_width),:) = [];
+figure, imshow(img);
